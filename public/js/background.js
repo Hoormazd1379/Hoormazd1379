f26936ee81946bea9adac9e7c8e1e9cce296075d
Hoormazd Pirayeshfar <hoormazd@pirayeshfar.ch>
2023-11-02 10:55:11 +0100

    update size on resize

--- public/js/background.js
+++ public/js/background.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
     camera.position.z = 5;
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     renderer.domElement.style.position = 'fixed';
     renderer.domElement.style.top = '0';
@@ -14,18 +13,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const cubes = [];
     const cubeCount = 285;
 
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    onWindowResize();
+
+    window.addEventListener('resize', onWindowResize, false);
+
 for (let i = 0; i < cubeCount; i++) {
     const boxSize = 0.01 + Math.random() * 0.1;
     const geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
 
-    // Generate a random base color for the cube
     const baseColor = new THREE.Color(Math.random(), Math.random(), Math.random());
 
     const materials = [];
 
     for (let j = 0; j < 6; j++) {
-        // Create slightly different shades of the base color for each face
-        const shade = 0.1 + Math.random() * 0.9; // Adjust the range as needed
+        const shade = 0.1 + Math.random() * 0.9;
         const color = new THREE.Color(
             baseColor.r * shade,
             baseColor.g * shade,
@@ -61,23 +68,20 @@ for (let i = 0; i < cubeCount; i++) {
 
     animate();
 
-    // Create a skybox
     const skyboxGeometry = new THREE.BoxGeometry(100, 100, 100);
     const skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide }); // Start with a black sky
     const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
     scene.add(skybox);
 
-    // Function to smoothly change the skybox color
     const changeSkyboxColor = () => {
         const randomDarkColor = getRandomDarkColor();
         new TWEEN.Tween(skybox.material.color)
             .to({ r: (randomDarkColor >> 16) / 255, g: ((randomDarkColor >> 8) & 0xFF) / 255, b: (randomDarkColor & 0xFF) / 255 }, 3000)
             .onUpdate(() => {
-                // Animate the color change
             })
             .start();
 
-        setTimeout(changeSkyboxColor, 2500); // Change color every 3 seconds
+        setTimeout(changeSkyboxColor, 2500);
     };
 
     const getRandomDarkColor = () => {
@@ -87,10 +91,8 @@ for (let i = 0; i < cubeCount; i++) {
         return (r << 16) | (g << 8) | b;
     };
 
-    // Start changing the skybox color
     changeSkyboxColor();
     
-    // Update Tween.js in the animation loop
     function animateTween() {
         requestAnimationFrame(animateTween);
         TWEEN.update();
